ed5d1afb04ff675055c917f70dd5c82356420462
niefanjun <niefanjun@zbj.com>
2018-01-02 16:47:22 +0800

    test

--- webnote/src/page/index.vue
+++ webnote/src/page/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>index page
   <img src="/static/image/test.png" alt="">
-  <a href="http://www.baidu.com" target="_blank"></a>
-  <a href="http://www.163.com" target="_blank"></a>
+  <a href="http://www.baidu.com" target="_blank">百度</a>
+  <a href="http://www.163.com" target="_blank">网易</a>
   </div>
 </template>
