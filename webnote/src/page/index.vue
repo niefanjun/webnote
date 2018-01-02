057c43243b8b7ccaff49dfe685263857f2b94c18
niefanjun <niefanjun@zbj.com>
2018-01-02 15:48:23 +0800

    test

--- webnote/src/page/index.vue
+++ webnote/src/page/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>index page
   <img src="/static/image/test.png" alt="">
-  <script src="/static/js/UE.js"></script>
   </div>
 </template>
+<script src="/static/js/UE.js"></script>
