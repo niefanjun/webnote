f99f61d73f2e014bc9fec25af064c2c4985041be
niefanjun <niefanjun@zbj.com>
2018-01-02 15:47:06 +0800

    test

--- webnote/src/App.vue
+++ webnote/src/App.vue
@@ -8,7 +8,6 @@ export default {
   name: 'app'
 }
 </script>
-<script src="/static/js/UE.js"></script>
 <style lang="scss">
 @import "./style/style";
 </style>
--- webnote/src/page/index.vue
+++ webnote/src/page/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>index page
   <img src="/static/image/test.png" alt="">
+  <script src="/static/js/UE.js"></script>
   </div>
 </template>
